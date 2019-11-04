4203b3b5ddadc9cc55b24449a8816b00bc63fbb9
Tobias Ritscher <tobirit@gmail.com>
2019-11-04 08:41:33 +0100

    some corrections in the Code of Erman (private and public) Added main method added a test input to see if the code is working

--- source/src/Editor.java
+++ source/src/Editor.java
@@ -3,9 +3,19 @@ import java.util.*;
 public class Editor {
 	private Map<Integer, String> paragraphs = new HashMap<>();
 	private Input input;
+
 	public Editor() {
-		for(int i = 0; i < input.splittext.size(); i++) {
-			paragraphs.put( i + 1, input.splittext.get(i));
+		for(int i = 0; i < input.getArrayList().size(); i++) {
+			paragraphs.put(i + 1, input.getArrayList().get(i));
 		}
+	}
+
+	public static void main(String[] args){
+		//MARK: only for testing
+		String randomString = "Das Projekt isch voll geil\nIch freu mich scho uf Siedler\nWer das liest ist doof!";
+		Input textInput = new Input(randomString);
+		textInput.printNumberedParagrap();
+
+		//TODO: Real Code
 	}
 }
--- source/src/Input.java
+++ source/src/Input.java
@@ -1,19 +1,21 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.*;
 	
 public class Input {
-	public List<String> splittext = new ArrayList<>();
+	private List<String> splittext = new ArrayList<>();
 	
-	private Input(String input) {
-		splittext = Arrays.asList(input.split("\\r?\\n"));		
+	public Input(String input) {
+		splittext = Arrays.asList(input.split("\n"));
 	}
 	
-	public List<String> getArrayList () {
+	public List<String> getArrayList() {
 		return splittext;
 	}
-	public void numberedParagrap () {
+
+	public void printNumberedParagrap() {
 		for(int i = 0; i < splittext.size(); i++) {
-			System.out.println((i + 1) + ":" + splittext.get(i));
+			System.out.println((i + 1) + ": " + splittext.get(i));
 		}
 	}
-	
 }
