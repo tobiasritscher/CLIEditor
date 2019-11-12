796e7ddd3af81ec83356632eea8fec942655c756
Tobias Ritscher <tobirit@gmail.com>
2019-11-12 09:49:47 +0100

    imported arrays in editor class and some small design upgrades

--- source/src/Editor.java
+++ source/src/Editor.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Arrays;
 
 /**
  * This Class handels all the editing of the text
@@ -14,154 +15,158 @@ public class Editor {
     private static Map<String, List<Integer>> wordIndex = new TreeMap<>();
     private static int ARRAY_OFFSET = 1;
 
-	/**
-	 * Constructor of the class Editor
-	 */
-	public Editor() {
+    /**
+     * Constructor of the class Editor
+     */
+    public Editor() {
         paragraphs = new ArrayList<>();
         input = new Input();
         output = new Output();
         loremIpsum = new LoremIpsum();
-	}
+    }
 
-	/**
-	 * geter for the paragraph List
-	 * @return all paragraphs as an list
-	 */
-	public static List<String> getParagraphs() {
-		return paragraphs;
-	}
+    /**
+     * geter for the paragraph List
+     *
+     * @return all paragraphs as an list
+     */
+    public static List<String> getParagraphs() {
+        return paragraphs;
+    }
 
-	/**
-	 * let you set a text you want to edit
-	 * @param userInput String to decide wich text to use
-	 */
-	public void chooseAndSetText(String userInput) {
-	    if (userInput.equalsIgnoreCase("Y")) {
+    /**
+     * let you set a text you want to edit
+     *
+     * @param userInput String to decide wich text to use
+     */
+    public void chooseAndSetText(String userInput) {
+        if (userInput.equalsIgnoreCase("Y")) {
             output.print("Please insert your text. At the end of your text switch to a new line and write 'END':");
             paragraphs = input.readInput();
         } else {
-	        paragraphs = loremIpsum.getTextArray();
+            paragraphs = loremIpsum.getTextArray();
         }
     }
 
-	/**
-	 * inserts a choosen paragraph at a choosen position
-	 * @param positionString position of the new paragrpah as String
-	 * @param newParagraph paragraph to set as String
-	 */
-	public void insertParagraph(String positionString, String newParagraph) {
-		int position = Integer.parseInt(positionString) - ARRAY_OFFSET;
-		paragraphs.add(position, newParagraph);
+    /**
+     * inserts a choosen paragraph at a choosen position
+     *
+     * @param positionString position of the new paragrpah as String
+     * @param newParagraph   paragraph to set as String
+     */
+    public void insertParagraph(String positionString, String newParagraph) {
+        int position = Integer.parseInt(positionString) - ARRAY_OFFSET;
+        paragraphs.add(position, newParagraph);
     }
 
-	/**
-	 * delets a pragraph at the choosen position
-	 * @param positionString position of the paragraph to delete as String
-	 */
-	public void deleteParagraph(String positionString) {
-    	int position = Integer.parseInt(positionString) - ARRAY_OFFSET;
-    	paragraphs.remove(position);
+    /**
+     * delets a pragraph at the choosen position
+     *
+     * @param positionString position of the paragraph to delete as String
+     */
+    public void deleteParagraph(String positionString) {
+        int position = Integer.parseInt(positionString) - ARRAY_OFFSET;
+        paragraphs.remove(position);
     }
 
-	/**
-	 * replaces a choosen String with a new one
-	 */
-	public void replace() {
+    /**
+     * replaces a choosen String with a new one
+     */
+    public void replace() {
         //TODO: keyword suchen und ersetzen
 
     }
 
-	/**
-	 * prints an index of all words with the number of occurence and where to find them
-	 */
-	public void indexWords() {
-    	System.out.println("Total number of paragraphs: " + paragraphs.size());
-    	System.out.println("*******************************");
-    	
-    	
-    	for(int i = 0; i < paragraphs.size(); i++) {
-			String[] words = paragraphs.get(i).split(" ");
+    /**
+     * prints an index of all words with the number of occurence and where to find them
+     */
+    public void indexWords() {
+        System.out.println("Total number of paragraphs: " + paragraphs.size());
+        System.out.println("*******************************");
 
-			for (String s : words) {
-				String word = s.replaceAll("[,.]", "");
-				word = word.toLowerCase();
 
-				if (wordIndex.get(word) == null) {
-					wordIndex.put(word, new ArrayList<>());
+        for (int i = 0; i < paragraphs.size(); i++) {
+            String[] words = paragraphs.get(i).split(" ");
+
+            for (String s : words) {
+                String word = s.replaceAll("[,.]", "");
+                word = word.toLowerCase();
+
+                if (wordIndex.get(word) == null) {
+                    wordIndex.put(word, new ArrayList<>());
     				
     				/*
     				Das erste Element des Arrays zählt die Häufigkeit des Wortes.
     				Hier wird es initialisiert.
     				*/
-					wordIndex.get(word).add(1);
-    				
-    				// Das zweite Element des Arrays gibt an, in welchem Paragraph das Wort zum ersten Mal auftritt
-					wordIndex.get(word).add(i + 1);
+                    wordIndex.get(word).add(1);
 
-				} else {
-					//wordIndex.put(word,wordFrequency + 1);
-					wordIndex.get(word).set(0, wordIndex.get(word).get(0) + 1);
+                    // Das zweite Element des Arrays gibt an, in welchem Paragraph das Wort zum ersten Mal auftritt
+                    wordIndex.get(word).add(i + 1);
 
-					//Fügt hinzu, in welchem Paragraphen das Wort noch auftritt
-					wordIndex.get(word).add(i + 1);
-				}
-			}
-		}
-		printIndex();
+                } else {
+                    //wordIndex.put(word,wordFrequency + 1);
+                    wordIndex.get(word).set(0, wordIndex.get(word).get(0) + 1);
+
+                    //Fügt hinzu, in welchem Paragraphen das Wort noch auftritt
+                    wordIndex.get(word).add(i + 1);
+                }
+            }
+        }
+        printIndex();
     }
 
     private void indexInParagraph() {
-		//TODO: Speichert in welchem Paragraph sich das gesuchte Wort befindet
+        //TODO: Speichert in welchem Paragraph sich das gesuchte Wort befindet
     }
-    
+
     private void printIndex() {
-        for (Map.Entry<String, List<Integer>> word : wordIndex.entrySet()){
-        	String key = word.getKey();
-        	List<Integer> values = word.getValue();
-        	
-        	//Nur ausgeben, wenn das Wort häufiger als ein mal vorkommt
-        	if (values.get(0) > 1) {
-        		System.out.print("'" + key + "'");
-        		System.out.print(" exists " + values.get(0) + " times in paragraphs [");
-        		for (int i = 1; i < values.size() - ARRAY_OFFSET;i++) {
-        			System.out.print(values.get(i) + ", ");
-        		}
-        		//Print last occurrence of word
-        		System.out.println(values.get(values.size() - ARRAY_OFFSET) + "]");
-        	}
+        for (Map.Entry<String, List<Integer>> word : wordIndex.entrySet()) {
+            String key = word.getKey();
+            List<Integer> values = word.getValue();
+
+            //Nur ausgeben, wenn das Wort häufiger als ein mal vorkommt
+            if (values.get(0) > 1) {
+                System.out.print("'" + key + "'");
+                System.out.print(" exists " + values.get(0) + " times in paragraphs [");
+                for (int i = 1; i < values.size() - ARRAY_OFFSET; i++) {
+                    System.out.print(values.get(i) + ", ");
+                }
+                //Print last occurrence of word
+                System.out.println(values.get(values.size() - ARRAY_OFFSET) + "]");
+            }
         }
-	}
+    }
 
     /**
      * TODO: Formatierte Ausgabe von Text mit einer einstellbaren maximalen Spaltenbreite Zeichen,
-     *         Zeilenumbruch jeweils auf dem letzten Lehrzeichen
-     *         (bei zu langen Wörtern an beliebiger Stelle innerhalb des Wortes)
+     * Zeilenumbruch jeweils auf dem letzten Lehrzeichen
+     * (bei zu langen Wörtern an beliebiger Stelle innerhalb des Wortes)
      */
-       public void formatedText(String lengthInput) {
-     int paragraphLength = Integer.parseInt(lengthInput);
-     List<String> wordList = words(paragraphs);
+    public void formatedText(String lengthInput) {
+        int paragraphLength = Integer.parseInt(lengthInput);
+        List<String> wordList = words(paragraphs);
 
 
-       }
+    }
 
-       private List<String> words(List<String> text){
-     String textString = listToString(text);
+    private List<String> words(List<String> text) {
+        String textString = listToString(text);
 
-     return Arrays.asList(textString.split(" "));
+        return Arrays.asList(textString.split(" "));
     }
 
     private String listToString(List<String> text) {
-     StringBuilder wordsBuilder = new StringBuilder();
+        StringBuilder wordsBuilder = new StringBuilder();
 
-     for (String value : text) {
-      String[] fulltext = value.split(" ");
-      for (String s : fulltext) {
-       wordsBuilder.append(s);
-       wordsBuilder.append(" ");
-      }
-     }
-     return wordsBuilder.toString();
+        for (String value : text) {
+            String[] fulltext = value.split(" ");
+            for (String s : fulltext) {
+                wordsBuilder.append(s);
+                wordsBuilder.append(" ");
+            }
+        }
+        return wordsBuilder.toString();
     }
 }
  
