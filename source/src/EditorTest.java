bd1f290dc64d83c6774e61709521522606f5d3d7
Tobias Ritscher <tobirit@gmail.com>
2019-11-13 09:18:25 +0100

    Corrected some work of Erman in Test class

--- source/src/EditorTest.java
+++ source/src/EditorTest.java
@@ -1,5 +1,3 @@
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
