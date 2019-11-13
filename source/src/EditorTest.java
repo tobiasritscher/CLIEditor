13498468d834ffdc2807c8545fdfaa752b5a078a
erman <zankoerm@students.zhaw.ch>
2019-11-13 09:13:43 +0100

    first steps in Testclass

--- source/src/EditorTest.java
+++ source/src/EditorTest.java
@@ -1,11 +1,12 @@
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 class EditorTest {
 
-	private static final ArrayList<>
+	
 	
 	@BeforeEach
 	public void SetUp() {
