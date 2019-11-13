ea78ab2bf164703b837c1577648f97d0db6bff1d
corrooli <oliver.corrodi@students.zhaw.ch>
2019-11-13 13:35:04 +0100

    Formatted source code to make it tidier

--- source/src/EditorTest.java
+++ source/src/EditorTest.java
@@ -1,11 +1,8 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
-
 import static org.junit.Assert.assertEquals;
-
 
 public class EditorTest {
     private Editor editor;
